952bf4c39b75959076c3708a607af3168fc05b13
odinokun <odinokun@gmail.com>
2023-05-29 09:41:44 +0200

    done less #03

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import './App.css';
+import { v1 } from 'uuid';
+
 import { Todolist } from './Todolist';
+
 import { IFilter } from './models/types';
+import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
+    { id: v1(), title: 'HTML&CSS', isDone: true },
+    { id: v1(), title: 'JS', isDone: true },
+    { id: v1(), title: 'ReactJS', isDone: false },
   ]);
   const [filter, setFilter] = useState<keyof IFilter>('all');
 
-  const removeTask = (id: number) => {
+  const removeTask = (id: string) => {
     setTasks(tasks.filter(task => task.id !== id));
   };
 
@@ -21,14 +24,24 @@ function App() {
   } else if (filter === 'completed') {
     tasks4Todo = tasks.filter(task => task.isDone);
   }
-
   const changeFilter = (value: keyof IFilter) => {
     setFilter(value);
   };
 
+  const addTask = (title: string) => {
+    const newTask = { id: v1(), title, isDone: false };
+    setTasks([newTask, ...tasks]);
+  };
+
   return (
     <div className='App'>
-      <Todolist title='What to learn?' tasks={tasks4Todo} removeTask={removeTask} changeFilter={changeFilter} />
+      <Todolist
+        title='What to learn?'
+        tasks={tasks4Todo}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+        addTask={addTask}
+      />
     </div>
   );
 }
